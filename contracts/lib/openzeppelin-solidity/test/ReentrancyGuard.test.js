085470d336388d1e8eca7545d2e1079209190a4a
hironobu ueno <ueno@doublejump.tokyo>
2018-09-22 17:54:36 +0900

    いろいろ修正

--- contracts/lib/openzeppelin-solidity/test/ReentrancyGuard.test.js
+++ contracts/lib/openzeppelin-solidity/test/ReentrancyGuard.test.js
@@ -1,36 +1,36 @@
-const { expectThrow } = require('./helpers/expectThrow');
-const ReentrancyMock = artifacts.require('ReentrancyMock');
-const ReentrancyAttack = artifacts.require('ReentrancyAttack');
+const { expectThrow } = require('./helpers/expectThrow')
+const ReentrancyMock = artifacts.require('ReentrancyMock')
+const ReentrancyAttack = artifacts.require('ReentrancyAttack')
 
-const BigNumber = web3.BigNumber;
+const BigNumber = web3.BigNumber
 
 require('chai')
   .use(require('chai-bignumber')(BigNumber))
-  .should();
+  .should()
 
-contract('ReentrancyGuard', function () {
-  let reentrancyMock;
+contract('ReentrancyGuard', function() {
+  let reentrancyMock
 
-  beforeEach(async function () {
-    reentrancyMock = await ReentrancyMock.new();
-    const initialCounter = await reentrancyMock.counter();
-    initialCounter.should.be.bignumber.equal(0);
-  });
+  beforeEach(async function() {
+    reentrancyMock = await ReentrancyMock.new()
+    const initialCounter = await reentrancyMock.counter()
+    initialCounter.should.be.bignumber.equal(0)
+  })
 
-  it('should not allow remote callback', async function () {
-    const attacker = await ReentrancyAttack.new();
-    await expectThrow(reentrancyMock.countAndCall(attacker.address));
-  });
+  it('should not allow remote callback', async function() {
+    const attacker = await ReentrancyAttack.new()
+    await expectThrow(reentrancyMock.countAndCall(attacker.address))
+  })
 
   // The following are more side-effects than intended behavior:
   // I put them here as documentation, and to monitor any changes
   // in the side-effects.
 
-  it('should not allow local recursion', async function () {
-    await expectThrow(reentrancyMock.countLocalRecursive(10));
-  });
+  it('should not allow local recursion', async function() {
+    await expectThrow(reentrancyMock.countLocalRecursive(10))
+  })
 
-  it('should not allow indirect local recursion', async function () {
-    await expectThrow(reentrancyMock.countThisRecursive(10));
-  });
-});
+  it('should not allow indirect local recursion', async function() {
+    await expectThrow(reentrancyMock.countThisRecursive(10))
+  })
+})
