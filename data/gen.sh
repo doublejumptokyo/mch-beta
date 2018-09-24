3af15a8d68aca47192d28cf7e18198e7ec3b037a
Ryo Manzoku <manzoku.ryo@gmail.com>
2018-09-24 08:48:23 +0000

    fix

--- data/gen.sh
+++ data/gen.sh
@@ -4,8 +4,10 @@ file="js/hero_mint.js"
 
 split -l100 -d raw/mint.txt splited/mint_
 
+prefix=200
+
 for f in `ls splited`; do
-    file="js/$(date +%s)_$f.js"
+    file="js/${prefix}_$f.js"
 
     echo -n "" > $file
     echo "let HeroManager = artifacts.require('HeroManager');" >> $file
@@ -18,7 +20,8 @@ for f in `ls splited`; do
     echo "    });" >> $file
     echo -n "}" >> $file
 
-    sleep 1
+    prefix=$((prefix+1))
+    # sleep 1
 done
 
 
