5dfbbf7947e02d7cdc9c3ad799ba0981bcad1976
hironobu ueno <ueno@doublejump.tokyo>
2018-09-27 19:17:21 +0900

    user update

--- contracts/user/User.sol
+++ contracts/user/User.sol
@@ -1,9 +1,11 @@
 pragma solidity 0.4.24;
 
+import "../lib/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract User {
 
-    event UserSetting (address indexed user, string name, string ipfs);
+contract User is Ownable {
+
+    event UserSetting_2 (address indexed user, string name, string ipfs);
 
     string public constant DEFAULT_NAME = "beta player";
 
@@ -22,7 +24,7 @@ contract User {
         // account.name = addressToString(msg.sender);
         account.name = DEFAULT_NAME;
         
-        emit UserSetting (msg.sender, account.name, account.ipfs);
+        emit UserSetting_2 (msg.sender, account.name, account.ipfs);
     }
 
     function setName(string _name) public {
@@ -30,16 +32,26 @@ contract User {
         require(account.exists);     
         account.name = _name;
 
-        emit UserSetting (msg.sender, account.name, account.ipfs);
-    } 
+        emit UserSetting_2 (msg.sender, account.name, account.ipfs);
+    }
 
     function setIpfs(string _ipfs) public {
         Account storage account = accounts[msg.sender];   
         require(account.exists);     
         account.ipfs = _ipfs;
 
-        emit UserSetting (msg.sender, account.name, account.ipfs);
-    }     
+        emit UserSetting_2 (msg.sender, account.name, account.ipfs);
+    }
+
+    function set(address _address, string _name, string _ipfs) public onlyOwner {
+        Account storage account = accounts[_address];   
+
+        account.name = _name;
+        account.ipfs = _ipfs;
+        account.exists = true;
+
+        emit UserSetting_2 (_address, account.name, account.ipfs);
+    }
 
     function getAccount() public view returns (
         bool exists,
@@ -51,6 +63,15 @@ contract User {
             return (true, account.name, account.ipfs);
         }
         return (false, "", "");
+    }
+
+    function get(address _address) public view returns (
+        bool exists,
+        string  name,
+        string  ipfs
+    ) {
+        Account storage account = accounts[_address];
+        return (account.exists, account.name, account.ipfs);
     }
 
     function addressToString(address _address) public pure returns (string) {
