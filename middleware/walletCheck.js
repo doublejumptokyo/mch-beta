acb52afa07f01ca210fab44403d1ccae547bd748
Yosuke Tanaka <yosket87@gmail.com>
2018-09-27 11:13:49 +0900

    エラー画面修正

--- middleware/walletCheck.js
+++ middleware/walletCheck.js
@@ -1,6 +1,11 @@
 export default function({ store, error }) {
   if (process.server) return
-  if (!store.getters.isLoggedIn) {
+  if (!store.state.hasWallet) {
     error({ statusCode: 401, message: 'No wallet.' })
+    return
+  }
+  if (!store.getters.isLoggedIn) {
+    error({ statusCode: 401, message: 'Not logged in.' })
+    return
   }
 }
