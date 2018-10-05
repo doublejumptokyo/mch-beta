23ee5b37ce739561e81ba7d3984b324559fe23fa
hironobu ueno <ueno@doublejump.tokyo>
2018-10-05 18:05:29 +0900

    present開放

---
+++ plugins/dc-present.js
@@ -0,0 +1,29 @@
+import PresentData from '~/build/contracts/Present.json'
+
+class Present {
+  constructor(accountManager) {
+    const presentAbi = PresentData.abi
+    const presentAddress = PresentData.networks['default'].address
+    this.contract = accountManager.getContract(presentAbi, presentAddress)
+  }
+
+  async canReceive() {
+    return await this.contract.methods.canReceive().call()
+  }
+
+  async receive() {
+    return new Promise((resolve, reject) => {
+        this.contract.methods
+          .receive()
+          .send()
+          .on('receipt', resolve)
+          .on('error', reject)
+      })
+  }
+}
+
+export default async ({ app, store }, inject) => {
+  if (!store.getters.isLoggedIn) return
+  const present = new Present(app.$accountManager)
+  inject('present', present)
+}
