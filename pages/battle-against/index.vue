191663fead427fa0d7ccc0b46a41c7019c680e1c
Yosuke Tanaka <yosket87@gmail.com>
2018-10-11 16:53:50 +0900

    対戦相手のリフレッシュ機能

--- pages/battle-against/index.vue
+++ pages/battle-against/index.vue
@@ -1,8 +1,11 @@
 <template lang="pug">
 .battlePage
   .page__title
-    h1 {{ $i18n.t('pages.battles') }}
-  p Select a user to play against.
+    div
+      h1 {{ $i18n.t('pages.battles') }}
+      p Select a user to play against.
+    button(@click="fetch")
+      fa-icon(icon="sync")
   user-list(:users="users")
 </template>
 
@@ -21,19 +24,54 @@ export default {
     }
   },
   computed: mapState(['loomAddress']),
-  async beforeMount() {
-    const from = Math.floor(Math.random() * TOTAL_USER_COUNT) + 1
-    const addresses = await this.$rank.list(from)
-    const users = []
-    await Promise.all(
-      addresses
-        .filter(address => address !== this.loomAddress)
-        .map(async address => {
-          const user = await this.$user.get(address)
-          users.push({ name: user.name, address })
-        })
-    )
-    this.$set(this, 'users', users)
+  beforeMount() {
+    this.fetch()
+  },
+  methods: {
+    async fetch() {
+      const from = Math.floor(Math.random() * TOTAL_USER_COUNT) + 1
+      const addresses = await this.$rank.list(from)
+      const users = []
+      await Promise.all(
+        addresses
+          .filter(address => address !== this.loomAddress)
+          .map(async address => {
+            const user = await this.$user.get(address)
+            users.push({ name: user.name, address })
+          })
+      )
+      this.$set(this, 'users', users)
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.battlePage {
+  .page__title {
+    align-items: center;
+    display: flex;
+
+    > div {
+      flex: 1;
+    }
+
+    h1 {
+      font-size: 1.2rem;
+    }
+
+    p {
+      font-size: 0.8rem;
+    }
+
+    button {
+      border: 1px solid map-get($colors, primary);
+      border-radius: 1rem;
+      color: map-get($colors, primary);
+      font-size: 0.8rem;
+      line-height: 1;
+      padding: 0.5rem;
+    }
+  }
+}
+</style>
