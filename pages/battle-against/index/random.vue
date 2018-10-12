60ec43b6f556904bc70b1c822b72d9c3dd1b8a09
Yosuke Tanaka <yosket87@gmail.com>
2018-10-12 20:38:57 +0900

    おおかた実装

---
+++ pages/battle-against/index/random.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.battlePage
+  user-list(:users="users")
+</template>
+
+<script>
+import UserList from '~/components/UserList'
+
+const TOTAL_USER_COUNT = 415 - 20
+
+export default {
+  components: { UserList },
+  props: {
+    users: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  beforeMount() {
+    const from = Math.floor(Math.random() * TOTAL_USER_COUNT) + 1
+    this.$emit('init', { from })
+  }
+}
+</script>
