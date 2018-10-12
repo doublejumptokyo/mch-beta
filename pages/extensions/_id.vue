9992220cab67e9ea79fb95248d2c64448b87e1eb
hilarious23 <s.positex11@gmail.com>
2018-10-12 15:45:38 +0900

    modify item id css

--- pages/extensions/_id.vue
+++ pages/extensions/_id.vue
@@ -10,6 +10,7 @@
     .assetPage__name
       h1 {{ item.name[$i18n.locale] }}
       p {{ `#${item.id}` }}
+
   .assetPage__right(v-if="item")
     .assetPage__statuses
       .assetPage__status
@@ -84,6 +85,11 @@ export default {
   &__name {
     margin: 0 0 1rem;
     text-align: center;
+
+    p {
+      color: #999;
+      font-weight: 100;
+    }
   }
 
   &__rarity {
