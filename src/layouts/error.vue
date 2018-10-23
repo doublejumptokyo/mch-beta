215b533453680e2027f9b0b48b1f60612a1fd645
Yosuke Tanaka <yosket87@gmail.com>
2018-10-23 19:09:42 +0900

    paramsのvalidateと404ページ

--- src/layouts/error.vue
+++ src/layouts/error.vue
@@ -1,6 +1,12 @@
 <template lang="pug">
 .errorPage
   no-wallet(v-if="error.statusCode === 401" :message="error.message")
+  .errorPage__template(v-else-if="error.statusCode === 404")
+    h2 404
+    p {{ error.message }}
+  .errorPage__template(v-else)
+    h2 Error
+    p {{ error.message }}
 </template>
 
 <script>
@@ -17,3 +23,25 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.errorPage {
+  &__template {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    justify-content: center;
+
+    h2 {
+      color: #666;
+      font-size: 6rem;
+    }
+
+    p {
+      color: #999;
+      font-size: 1.2rem;
+    }
+  }
+}
+</style>
